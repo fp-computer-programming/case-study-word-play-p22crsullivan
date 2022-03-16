ec584ef6233d02e2f2b5295952395d668a33d296
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-03-16 10:18:13 -0400

    Save Cycle 14

--- word_play-1.py
+++ word_play-1.py
@@ -1,7 +1,7 @@
 # Author: CRS 03/16/22
 ofile = open("words.txt", "r+")
 new_file = open("greater_than_20.txt", "w")
-contents = ofile.readline()
+contents = ofile.readlines()
 if len(contents) > 20:
     new_file.write(contents)
 ofile.close()
