03b41ab4c6bc65a156e1459757bd38f90f6b16fb
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-03-23 11:06:17 -0400

    Save Cycle 14

--- word_play-3.py
+++ word_play-3.py
@@ -1,13 +1,32 @@
 # Author: CRS 03/16/22
-# Open file and read lines
-file = open("words.txt")
-lines = file.readline()
-def avoid(forbidden_letters):
-    acceptable_counter = 0
-    for line in file:
-        if forbidden_letters not in line:
-            acceptable_counter += 1
-        break
-    print('{0} acceptable words found.'.format(avoid(acceptable_counter)))  
-
-print(avoid(str(input('Enter non acceptable letters: '))))
+# Define avoid function
+def avoid(string,letters):
+    # Open file
+    with open("words.txt") as infile:
+        words_with = 0
+        # Create loop to add to counter
+        for wordLetter in letters:
+            counter = 0 
+            found = False
+            for letter in string:
+                if letter != wordLetter:
+                    counter += 1
+                    if counter == len(string):
+                        found = False
+                else:
+                    found = True
+            if found == True:
+                return True
+                # Prompt user for forbidden letters
+        forbidden_letters = input("Input Letters that you do not want from the txt file: ")
+        # Create for loop to check for forbidden letters
+        for line in infile:
+            counter = 0
+            for letter in line:
+                if letter != forbidden_letters:
+                    counter += 1
+                    if counter == len(line):
+                        words_with += 1
+                        break
+# Run code
+print(avoid("yep", "yep"))
