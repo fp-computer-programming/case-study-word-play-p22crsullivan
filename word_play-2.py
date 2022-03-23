8f68d0fec2654a1f1df9a1582c3c0ea13ed2bdd4
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-03-23 10:54:17 -0400

    Save Cycle 14

--- word_play-2.py
+++ word_play-2.py
@@ -1,1 +1,29 @@
 # Author: CRS 03/16/22
+# Define function
+def number_e():
+    # Open words.txt
+    with open("words.txt") as infile:
+        # Open  words_without_e.txt
+        outfile = open("words_without_e.txt", "w")
+        # Set variables
+        word_count = 0
+        words_without_e = 0
+        # Create for loops to check for an e
+        for l in infile:
+            not_e = False
+            counter = 0
+            word_count += 1
+            for letter in l:
+                if letter != "e":
+                    counter += 1
+                    if counter == len(l):
+                        words_without_e += 1
+                        not_e = True
+                        break
+            if not_e == True:
+                outfile.write(l + "\n")
+    # Print the percentage
+    percent = (words_without_e / word_count) * 100
+    print("Percent of words without e: {0}".format(percent))
+# Test the code
+number_e()
--- word_play-3.py
+++ word_play-3.py
@@ -1,1 +1,13 @@
 # Author: CRS 03/16/22
+# Open file and read lines
+file = open("words.txt")
+lines = file.readline()
+def avoid(forbidden_letters):
+    acceptable_counter = 0
+    for line in file:
+        if forbidden_letters not in line:
+            acceptable_counter += 1
+        break
+    print('{0} acceptable words found.'.format(avoid(acceptable_counter)))  
+
+print(avoid(str(input('Enter non acceptable letters: '))))
